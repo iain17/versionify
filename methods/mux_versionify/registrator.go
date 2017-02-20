c9021be39d505ab4cda05c05ad81f91e4d30b269
Iain Munro <iain17@me.com>
2017-02-20 20:37:14 +0100

    Wrote mux registrator

--- methods/mux_versionify/registrator.go
+++ methods/mux_versionify/registrator.go
@@ -2,10 +2,19 @@ package mux_versionify
 
 import (
 	"github.com/Pocketbrain/versionify"
+	"github.com/gorilla/mux"
 	"fmt"
 )
 
-func Registrator(version *versionify.Version, methods versionify.Methods) {
-	fmt.Println(version.String())
-	fmt.Println(methods)
+func NewRegistrator(router *mux.Router) versionify.VersionRegistrator {
+	//Called for each version.
+	return func (version *versionify.Version, methods versionify.Methods) {
+		path := "/"+version.String()
+		fmt.Printf("Registering sub route '%s'\n", path)
+		subRouter := router.PathPrefix(path).Subrouter()
+		for name, _method := range methods {
+			fmt.Printf("Registering handler '%s'\n", name)
+			_method.(*MuxMethod).registerFunc(subRouter)
+		}
+	}
 }
